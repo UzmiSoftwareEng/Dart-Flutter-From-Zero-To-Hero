7e3f9f2e7aec5965d5ab99b2fb8bdabd991517d9
UzmiSoftwareEng <numl-s25-28232@numls.edu.pk>
2026-08-05 13:33:19 +0500

    Operators in dart

--- Day4/Comments.dart
+++ Day4/Comments.dart
@@ -1,7 +1,7 @@
 void main(){
   //comments are used to leave notes for yourself 
   //String name = "Uzma Mumtaz";
-  int age = 20;
+  //int age = 20;
   //print(name);
-  print(age);
+  //print(age);
 }
