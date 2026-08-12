3522a840f231bbb6d3eeda555feb29e82beff489
UzmiSoftwareEng <numl-s25-28232@numls.edu.pk>
2026-08-12 13:58:26 +0500

    Maps in dart

---
+++ Day15/MapInDart.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  //Maps-->Key-value pairs
+  //Map map = {"name":"Uzma Mumtaz","age":"19"};
+  Map map = Map();
+  //print(map["age"]);
+  //map["education"] = "Graduate";
+  map["bio"] = "i am developer";
+  print(map);
+  //map.remove("education");
+  //map.clear();
+  map.addAll({"Hobby": "photography"});
+
+  //print(map.length);
+  print(map.keys);
+  print(map.values);
+  print(map);
+}
