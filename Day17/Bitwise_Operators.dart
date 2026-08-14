acf938013925dfbb38a75c26e8995be6b1da6978
UzmiSoftwareEng <numl-s25-28232@numls.edu.pk>
2026-08-14 16:15:37 +0500

    Bitwise operators in dart

---
+++ Day17/Bitwise_Operators.dart
@@ -0,0 +1,22 @@
+void main(){
+  
+  //Example 1
+  //int num1 = 6;
+  // int num2 = 3;
+  //int and= num1 & num2;
+  //print("AND bitwise operator $and");
+
+  //Example 2
+   int a = 5;
+  int b = 3;
+  int and= a & b;
+  print("AND bitwise operator $and");
+
+  int or= a | b;
+  print("or bitwise operator $or");
+int not= ~a;
+  print("AND bitwise operator $not");
+int xor= a ^ b;
+  print("AND bitwise operator $xor");
+
+}
