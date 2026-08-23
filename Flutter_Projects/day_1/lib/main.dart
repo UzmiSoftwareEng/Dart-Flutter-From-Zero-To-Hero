d3596ebd5404781b742a1dcac5612bdf2b78718f
UzmiSoftwareEng <numl-s25-28232@numls.edu.pk>
2026-08-24 00:40:37 +0500

    ListView and its keywords in flutter

--- Flutter_Projects/day_1/lib/main.dart
+++ Flutter_Projects/day_1/lib/main.dart
@@ -1,36 +1,8 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MyApp());
-  
-}
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      title: "My App",
-      theme: ThemeData(primarySwatch:Colors.blue),
-      home: HomeScreen(),
-    );
-  }
-  
-}
-class HomeScreen extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(title:Text("Home"),
-      actions: [IconButton(icon:Icon (Icons.search),onPressed: (){},)],
-      ),
-      drawer: Drawer(),
-      body: Center(
-        child: Text("Hello Flutter"),
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: (){},
-      child: Icon((Icons.add)),),
-      bottomNavigationBar: BottomNavigationBar(items: [],),
-    );
-
-  }
+void main() {
+  runApp(
+      MaterialApp(
+          home: Text("Hello World"),
+      )
+  );
 }
--- Flutter_Projects/day_3/lib/main.dart
+++ Flutter_Projects/day_3/lib/main.dart
@@ -3,57 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-      theme: ThemeData(
-
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Container'),
-
-      ),
-
-      body: Center(
-        child: Container(
-          width: 100,
-          height: 150,
-          color: Colors.lightGreen,
-          child: Text('Hello im uzma imtiaz'),
-
+        Scaffold(
+            appBar: AppBar(
+              title: Text('Flutter Logo'),
+            ),
+            body: Images.assests('image/logo.png')
 
         ),
-
-      )
-
     );
   }
 }
