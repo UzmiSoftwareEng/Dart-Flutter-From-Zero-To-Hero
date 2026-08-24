7c7f726d0c77de10f277c1d97944f7c3f2c53f95
UzmiSoftwareEng <numl-s25-28232@numls.edu.pk>
2026-08-24 21:25:12 +0500

    Adding decoration to container in flutter

---
+++ Flutter_Projects/day_14/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DecoretionApp());
+}
+
+class DecoretionApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     home: ContainerDecorationApp(),
+   ); //MaterialApp
+  }
+}
+
+class ContainerDecorationApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Demo Page"),
+        backgroundColor: Colors.pink,
+      ),
+    body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.lightBlue.shade100,
+      child: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+              border: Border.all(
+              width: 5,
+              color: Colors.black,
+          ),
+              boxShadow: [
+                BoxShadow(
+              blurRadius: 15,
+                  spreadRadius: 7,
+          )
+              ],
+            shape: BoxShape.circle
+        ),
+      ), //Container
+    ),
+    ),//Container
+); //Scaffold
+}
+}
