1db520c24d5214be930da0c3ad59e54ba2bf02e2
UzmiSoftwareEng <numl-s25-28232@numls.edu.pk>
2026-08-27 01:29:41 +0500

    Add custom font over text in flutter(Cleared)

---
+++ Flutter_Projects/day_20/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+),
+);
+  }
+}
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('Flutter Developer'),
+backgroundColor: Colors.pink.shade500,
+      ),
+      body: Text("Hello Everyone"),
+    );
+}}
+
