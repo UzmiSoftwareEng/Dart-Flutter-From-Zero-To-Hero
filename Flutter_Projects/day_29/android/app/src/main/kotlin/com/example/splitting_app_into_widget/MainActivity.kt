af27b8bd53784d847cb429aec403031f9affd7dd
UzmiSoftwareEng <numl-s25-28232@numls.edu.pk>
2026-09-01 17:56:26 +0500

    Splitting App into Widgets in flutter Example 1 using Profile() and LoginButton()

---
+++ Flutter_Projects/day_29/android/app/src/main/kotlin/com/example/splitting_app_into_widget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.splitting_app_into_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Flutter_Projects/day_29/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(SplittingFlutterApp());
+}
+class SplittingFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DashBoardScreen(),
+    );
+  }
+}
+
+class ProfileWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            radius: 50,
+          ),
+        ),
+        Text("Uzma Mumtaz", style: TextStyle(fontSize: 20),),
+        Text('Future Flutter Developer'),
+      ],
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: (){
+          print("Logged In");
+          SizedBox(height: 70);
+        },
+        child: Text('Login'),
+    );
+  }
+}
+
+class DashBoardScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Developer"),
+        backgroundColor: Colors.grey,
+      ),
+
+      body: Column(
+        children: [
+          ProfileWidget(),
+          LoginButton(),
+        ],
+      ),
+    );
+  }
+}
