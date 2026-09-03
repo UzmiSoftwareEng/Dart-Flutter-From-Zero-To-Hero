2c8cbf37b5efbff04791efee6785fd2244b0b12d
UzmiSoftwareEng <numl-s25-28232@numls.edu.pk>
2026-09-03 14:47:47 +0500

    How to add Font Awesome Icons in flutter adding latestversion in pupspec.yaml

---
+++ Flutter_Projects/day_37/android/app/src/main/kotlin/com/example/font_awesome_icon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.font_awesome_icon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Flutter_Projects/day_37/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(MyProject());
+}
+
+class MyProject extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Font Awesome Icons'),
+        backgroundColor: Colors.limeAccent,
+      ),
+      body:  Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.fax_rounded,
+            color: Colors.purple,
+            size: 50,
+            ),
+       SizedBox(width: 10,),
+       FaIcon(FontAwesomeIcons.instagram,
+         color: Colors.purple,
+         size: 50,
+      ),
+    ],
+        ),
+      ),
+    );
+  }
+}
