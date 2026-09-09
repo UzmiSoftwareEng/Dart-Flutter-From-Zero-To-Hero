04de7e07ca8de13459ad75087473337fd1e81b2a
UzmiSoftwareEng <numl-s25-28232@numls.edu.pk>
2026-09-09 12:09:01 +0500

    Adding Range Slider in flutter using RangeValues, RangeLabels,divisions, onChanged ,newValues(Cleared)

---
+++ Flutter_Projects/day_46/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.purple
+      ),
+      home: MyHomeScreen(),
+    );
+  }
+}
+
+class MyHomeScreen extends StatefulWidget {
+
+  @override
+  State<MyHomeScreen> createState() => _MyHomeScreenState();
+
+  }
+class _MyHomeScreenState extends State<MyHomeScreen> {
+   RangeValues values = RangeValues(0, 2);
+@override
+Widget build(BuildContext context) {
+  RangeLabels labels = RangeLabels(values.start.toString(), values.end.toString());
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('Range Slider'),
+        backgroundColor: Colors.purple,
+),
+  body: RangeSlider(
+      values: values,
+      labels: labels,
+      divisions: 10,
+      min: 0,
+      max: 2,
+      onChanged: (newValue) {
+        values = newValue;
+        print('${newValue.start}, ${newValue.end}');
+        setState(() {
+
+        });
+      }
+  ),
+);}}
