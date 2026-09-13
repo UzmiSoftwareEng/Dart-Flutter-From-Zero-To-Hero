c848abc54d9cd8f1d1ec3e21041da632df428c94
UzmiSoftwareEng <numl-s25-28232@numls.edu.pk>
2026-09-13 22:19:45 +0500

    Flutter Tween Animation deep understanding

---
+++ Flutter_Projects/day_56/lib/main.dart
@@ -0,0 +1,71 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blueGrey
+      ),
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+     with SingleTickerProviderStateMixin {
+   late Animation animation;
+   late Animation colorAnimation;
+   late AnimationController animationController;
+
+   @override
+   void initState() {
+    super.initState();
+    
+    animationController = AnimationController(vsync: this,
+        duration: Duration(seconds: 5));
+    animation = Tween(begin: 200.0, end: 100.0).animate(animationController);
+    colorAnimation = ColorTween(begin: Colors.orange, end: Colors.purple).animate(animationController  );
+
+    animationController.addListener(() {
+      print(animation.value);
+
+      setState(() {
+
+      });
+    });
+
+    animationController.forward();
+   }
+
+@override
+Widget build(BuildContext context) {
+   return Scaffold(
+    appBar: AppBar(
+    backgroundColor: Colors.green.shade300,
+    title: Text('Tween Animation'),
+),
+body: Center(
+  child: Container(
+      width: animation.value,
+      height: animation.value,
+    color: Colors.purple,
+  ),
+)
+   );
+   }
+}
